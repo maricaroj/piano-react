bcab3d674663f05ce310ab6b4fe230f876734597
maricaroj <juarezmariana18@gmail.com>
2021-06-05 12:21:00 -0300

    CREATE/creando repositorio del ejercicio piano-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import "./App.sass";
 
 function App() {
+  const notas = [
+    {
+      nombre: "Do",
+      link: require("./notes/do.wav"),
+      sostenido: true,
+    },
+    {
+      nombre: "Re",
+      link: require("./notes/re.wav"),
+      sostenido: true,
+    },
+    {
+      nombre: "Mi",
+      link: require("./notes/mi.wav"),
+    },
+    {
+      nombre: "Fa",
+      link: require("./notes/fa.wav"),
+      sostenido: true,
+    },
+    {
+      nombre: "Sol",
+      link: require("./notes/sol.wav"),
+      sostenido: true,
+    },
+    {
+      nombre: "La",
+      link: require("./notes/la.wav"),
+      sostenido: true,
+    },
+    {
+      nombre: "Si",
+      link: require("./notes/si.wav"),
+    },
+  ];
+
+  const handlerClickNote = (nota) => {
+    setSonando(nota.nombre)
+    const sonido = new Audio(nota.link.default);
+    sonido.play();
+  };
+
+  // const [click, setClick] = useState(0);
+  const [sonando, setSonando] = useState('');
+
+  // const handlerClickCount = () => {
+  //   setClick(click + 1);
+  // };
+
+  setTimeout(() => {
+    setSonando('');
+  },300)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="titulo">
+        <h1>Piano con React</h1>
+        <h2>Hecho por Mariana</h2>
+        <h3 className="esta-sonando">{sonando}</h3>
+        {/* <button onClick={handlerClickCount}>Has dado {click} clicks</button> */}
+      </div>
+      <div className="container">
+        {notas.map((nota) => (
+          <div key={nota.nombre} className={`nota ${sonando === nota.nombre && 'sonando'}`} onClick={() => handlerClickNote(nota)}>
+            {nota.sostenido && <div className="negra"></div>}
+            {/* {nota.sostenido ? <div className="negra"></div> : null} */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
